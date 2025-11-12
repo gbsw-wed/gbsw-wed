384d97a77643ee287805a623e1161393e048c08d
wjs_813 <jaeseongwi48@gmail.com>
2025-11-12 16:45:24 +0900

    back

---
+++ src/pages/myAbout.jsx
@@ -0,0 +1,24 @@
+import "./myAbout.css";
+
+function MyAbout() {
+  return (
+    <div>
+      <div className="mainBg">
+        <div className="mainBgCircle1"></div>
+        <div className="mainBgCircle2"></div>
+        <div className="myAboutContent">
+          <h4 className="myAboutTitle">내정보</h4>
+          <div className="myAboutImgBox">
+            <img src="/images/gbs-mascot.png" alt="" />
+            <label htmlFor="input-file" className="myAbout-input-file">
+              <p>프로필 변경</p>
+            </label>
+            <input type="file" id="input-file" style={{ display: "none" }} />
+            <button className="myAbout-input-del">계정 삭제</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MyAbout;
